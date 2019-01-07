12af202312861471adcc52bd9de9f23232448089
root <root@ip-172-31-6-213.eu-central-1.compute.internal>
2019-01-07 21:45:43 +0000

    removed docker run command

--- copy-to-local-docker.sh
+++ copy-to-local-docker.sh
@@ -7,5 +7,5 @@ cp bluecoat/default/eventgen.conf /docker/eventgen/configs/bluecoat/eventgen.con
 mkdir /docker/eventgen/samples
 cp bluecoat/samples/* /docker/eventgen/samples/
 
-docker run -v /docker/logfile1:/logfiles -v /docker/eventgen/configs:/configs -v /docker/eventgen/samples:/opt/splunk/etc/samples -i -t schose/eventgen:6.3.2 python -m splunk_eventgen -v generate /configs/bluecoat/eventgen.conf
-docker run -v /docker/logfile2:/logfiles -v /docker/eventgen/configs:/configs -v /docker/eventgen/samples:/opt/splunk/etc/samples -i -t schose/eventgen:6.3.2 python -m splunk_eventgen -v generate /configs/bluecoat/eventgen.conf
+#docker run -v /docker/logfile1:/logfiles -v /docker/eventgen/configs:/configs -v /docker/eventgen/samples:/opt/splunk/etc/samples -i -t schose/eventgen:6.3.2 python -m splunk_eventgen -v generate /configs/bluecoat/eventgen.conf
+#docker run -v /docker/logfile2:/logfiles -v /docker/eventgen/configs:/configs -v /docker/eventgen/samples:/opt/splunk/etc/samples -i -t schose/eventgen:6.3.2 python -m splunk_eventgen -v generate /configs/bluecoat/eventgen.conf
